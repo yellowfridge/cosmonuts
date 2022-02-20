17a8cc8843a4cf7f4a14c57782db03bd52b98ee4
redlentils <hsanli89@gmail.com>
2022-02-20 03:19:42 -0500

    added all base functionality - able to at least compile without errors

--- ethereum/contracts/CosmoNuts.sol
+++ ethereum/contracts/CosmoNuts.sol
@@ -1,10 +1,17 @@
-pragma solidity >=0.7.0;
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "./ERC721.sol";
+import "../extensions/ERC721Enumerable";
+import "../extensions/ERC721URIStorage.sol";
+import "../access/Ownable.sol";
 
 /**
   * @title CosmoNuts contract
+  * @custom: security-contact security@gmail.com //put security contact info (recommended)
   */
-contract CosmoNuts is ERC721, Ownable {
-  using SafeMath for uint256;
+contract CosmoNuts is ERC721, ERC721Enumerable, ERC721URIStorage, Ownable {
+  //using SafeMath for uint256; //Do you need to define this still - taking out for now - not needed for .8.0 and above
 
   string public CN_PROVENANCE = "";
   uint256 public constant nutPrice = 100000000000000000; //0.1 ETH BAYC had .08 ETH
@@ -16,13 +23,13 @@ contract CosmoNuts is ERC721, Ownable {
 
   constructor(string memory name, string memory symbol, uint256 maxNFTSupply, uint256 saleStart) ERC721(name, symbol) {
       MAX_NUTS = maxNFTSupply;
-      REVEAL_TIMESTAMP = saleStart + (86400 * 9) // Represents 9 days - from BAYC code (86,400 * 9) - showing exact amount to save gas
+      REVEAL_TIMESTAMP = saleStart + (86400 * 9); // Represents 9 days - from BAYC code (86,400 * 9) - should show exact amount to save gas
   }
 
   // Allows developer to withdraw funds
   function withdraw() public onlyOwner {
     uint balance = address(this).balance;
-    msg.sender.transfer(balance);
+    payable(msg.sender).transfer(balance);
   }
 
   // Set reveal timestamp
@@ -33,7 +40,7 @@ contract CosmoNuts is ERC721, Ownable {
 
   // Set initial provenance hash to prove initial set of images is same
   function setProvenanceHash(string memory provenanceHash) public onlyOwner {
-    BACY_PROVENANCE = provenanceHash;
+    CN_PROVENANCE = provenanceHash;
   }
 
   // Pause sale if active, make active if paused
@@ -43,19 +50,36 @@ contract CosmoNuts is ERC721, Ownable {
 
   // Set the baseuri where metadata will be stored
   // This is no longer how it is done - using URIStorage - go to newest implementation
-  function setBaseURI(string memory baseURI) public onlyOwner {
-    _setBaseURI(baseURI);
-  }
+  //function setBaseURI(string memory baseURI) public onlyOwner {
+  //  _setBaseURI(baseURI);
+  //}
+
+  
 
   // Mint Cosmo Nuts
   function mintNut(uint numberOfTokens) public payable {
     require(saleIsActive, "Sale must be active to mint Nut");
     require(numberOfTokens <= maxNutPurchase, "Can only mint 1 token at a time");
-    require(totalSupply().add(numberOfTokens) <= MAX_NUTS, "Purchase would exceed max supply of Nuts");
-    require(nutPrice.mul(numberOfTokens) <= msg.value, "Ether value sent is not correct");
+    require(totalSupply() + numberOfTokens <= MAX_NUTS, "Purchase would exceed max supply of Nuts");
+    require(nutPrice * numberOfTokens <= msg.value, "Ether value sent is not correct");
   }
 
-  //
+  // The following functions are overrides required by Solidity.
+  function _beforeTokenTransfer(address from, address to, uint256 tokenId) internal override(ERC721, ERC721Enumerable) {
+    super._beforeTokenTransfer(from, to, tokenId);
+  }
+
+  function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+    super._burn(tokenId);
+  }
+
+  function tokenURI(uint256 tokenId) public view override(ERC721, ERC721URIStorage) returns (string memory) {
+    return super.tokenURI(tokenId);
+  }
+
+  function supportsInterface(bytes4 interfaceId) public view override(ERC721, ERC721Enumerable) returns (bool) {
+    return super.supportsInterface(interfaceId);
+  }
 
 
 }
