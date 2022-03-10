3b01dfd20a1e087b270d870d7bb2b224da8023da
redlentils <hsanli89@gmail.com>
2022-03-10 04:20:24 -0500

    compile functionality added

--- ethereum/compile.js
+++ ethereum/compile.js
@@ -6,3 +6,14 @@ const buildPath = path.resolve(__dirname, 'build');
 fs.removeSync(buildPath);
 
 const contractPath = path.resolve(__dirname, 'contracts', 'CosmoNuts.sol')
+const source = fs.readFileSync(contractPath, 'utf8');
+const output = solc.compile(source, 1).contracts;
+
+fs.ensureDirSync(buildPath);
+
+for (let contract in output) {
+  fs.outputJsonSync(
+    path.resolve(buildPath, contract.replace(':', '') + '.json'),
+    output[contract]
+  );
+}
