7b81801b57e9a637d358c39c8ce996f6d92dc0fc
redlentils <hsanli89@gmail.com>
2022-03-16 03:38:42 -0400

    slowly trying out some stuff - published contract on Ropsten network

--- ethereum/web3_inf.js
+++ ethereum/web3_inf.js
@@ -4,6 +4,6 @@ import HDWalletProvider from '@truffle/hdwallet-provider';
 const mnemonic = "call glow acoustic vintage front ring trade assist shuffle mimic volume reject";
 const provider = new HDWalletProvider(mnemonic, "https://rinkeby.infura.io/v3/1d5e3588363b4f5a91ca15573760408c");
 
-const web3 = new Web3(provider);
+const web3_inf = new Web3(provider);
 
-export default web3;
+export default web3_inf;
--- pages/index.js
+++ pages/index.js
@@ -5,22 +5,33 @@ import detectEthereumProvider from '@metamask/detect-provider';
 import ImageGenerator from './imagegenerator';
 import QRCode from 'react-qr-code';
 import Mint from './mint';
+import Web3 from 'web3';
+import web3_inf from '../ethereum/web3_inf';
+import CosmoNuts from '../ethereum/build_manual/CosmoNuts_abi.json';
 
-// Latest deployed CosmoNuts address: 0xd8b934580fcE35a11B58C6D73aDeE468a2833fa8
+// Latest deployed CosmoNuts address: 0x66023f6da39cbffd7ad4f287ad4f8b44e0725167
+// https://ropsten.etherscan.io/tx/0xe207cdcc1a558b06f5790d409c222eb5fa1652f22a7a820c41a235b3b3a7094e
+// Deployed on Ropsten(3) network
 
 class Main extends Component {
   constructor(props) {
     super(props);
     this.state = {
       isUserConnected: null,
-      currentAccount: null
+      currentAccount: null,
+      isSaleActive: null
     };
 
     //this.connectUser = this.connectUser.bind(this);
   }
 
+  handleAccountChange(id) {
+    this.setState({ currentAccount: id })
+  }
+
   async componentDidMount() {
-    const provider = await detectEthereumProvider();
+    var provider = await detectEthereumProvider();
+    //console.log("Provider", provider);
 
     (() => {
       if (provider.selectedAddress === null) {
