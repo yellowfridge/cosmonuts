92c26ffe020a5cb5700e4f43594d8e1ea8136b25
redlentils <hsanli89@gmail.com>
2022-12-03 07:44:57 -0500

    added Butter/Seed - TreasuryStats - continuing to work on functionality

--- pages/universe.js
+++ pages/universe.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react';
 import Layout from '../components/layout';
 import Web3 from 'web3';
+import detectEthereumProvider from '@metamask/detect-provider';
 import CosmoUniverse from '../ethereum/build_manual/CosmoUniverse_abi.json';
 import CosmoNuts from '../ethereum/build_manual/CosmoNuts_abi.json';
 import CosmoTreasury from '../ethereum/build_manual/CosmoTreasury_abi.json';
-import { Input, Grid, Divider, Button, Statistic } from 'semantic-ui-react';
+import { Input, Grid, Divider, Button, Statistic, Form } from 'semantic-ui-react';
 
 class Universe extends Component {
   constructor() {
@@ -23,6 +24,7 @@ class Universe extends Component {
       initialMatter: 0,
       totalMatter: 0,
       systemMatter: '',
+      ownerMatter: '',
       treasuryMatter: '',
       matterOfAddress: '',
       matterOf: '',
@@ -30,8 +32,11 @@ class Universe extends Component {
       price: 0,
       rate: 0,
       butterJars: 0,
+      butterSpread: 0,
+      butterWeight: 0,
       seedsCreated: 0,
-      seedsGrown: 0
+      seedsGrown: 0, // Does not exist yet,
+      nutId: 0
     }
 
     this.collectUniverse = this.collectUniverse.bind(this);
@@ -40,6 +45,8 @@ class Universe extends Component {
     this.collectCosmo = this.collectCosmo.bind(this);
     this.changeMatterOfInput = this.changeMatterOfInput.bind(this);
     this.mintNut = this.mintNut.bind(this);
+    this.createButter = this.createButter.bind(this);
+    this.butterFormChange = this.butterFormChange.bind(this);
   }
 
   componentDidMount() {
@@ -122,6 +129,12 @@ class Universe extends Component {
       });
     });
 
+    await universe.methods.matterBalanceOf(this.state.owner).call().then((oMatter) => {
+      this.setState({
+        ownerMatter: oMatter
+      });
+    });
+
   }
 
   async collectTreasury() {
@@ -131,6 +144,30 @@ class Universe extends Component {
     await treasury.methods.getPrice().call().then((price) => {
       this.setState({
         price: price
+      });
+    });
+
+    await treasury.methods.butterJars().call().then((jars) => {
+      this.setState({
+        butterJars: jars
+      });
+    });
+
+    await treasury.methods.butterSpread().call().then((spread) => {
+      this.setState({
+        butterSpread: spread
+      });
+    });
+
+    await treasury.methods.butterWeight().call().then((weight) => {
+      this.setState({
+        butterWeight: weight
+      });
+    });
+
+    await treasury.methods.seedsCreated().call().then((createdSeeds) => {
+      this.setState({
+        seedsCreated: createdSeeds
       });
     });
   }
@@ -176,12 +213,45 @@ class Universe extends Component {
     const cosmos = new web3.eth.Contract(CosmoNuts, this.state.cosmosAddress);
     const provider = await detectEthereumProvider();
 
-    //await cosmos.methods.mintNut(this.state.supply).send({
-    //
-    //});
+    await cosmos.methods.mintNut(this.state.supply).send({
+      from: provider.selectedAddress,
+      value: this.state.price
+    }).on('transactionHash', function(hash) {
+      console.log("Transaction Hash:", hash);
+    }).on('receipt', function(receipt) {
+      console.log("Receipt", receipt);
+      alert("SUCCESS!");
+    }).on('error', function(error, receipt) {
+      console.log("Error:", error);
+      console.log("Receipt", receipt);
+      alert("Error!");
+    });
+  }
+
+  // HOW DO YOU DO THIS????
+  // Maybe need to pull in as a Form Group - that may have more of the options available
+  butterFormChange(event) {
+    console.log("event", event);
+    const formNames = [ 'nutId' ];
+    for (let i = 0; i < formNames.length; i++) {
+      var name = formNames[i];
+      this.setState({
+        name: event.target.value
+      });
+    }
+  }
+
+  async createButter(event) {
+    const web3 = new Web3(window.ethereum);
+    const cosmos = new web3.eth.Contract(CosmoNuts, this.state.cosmosAddress);
+    const provider = await detectEthereumProvider();
+
+    console.log("IN HERE BOI", event);
+    console.log("Trying to pulll Nut Id Value", event.target[0].value)
   }
 
   render() {
+
     return (
       <div>
 
@@ -190,9 +260,21 @@ class Universe extends Component {
           href="//cdnjs.cloudflare.com/ajax/libs/semantic-ui/2.2.12/semantic.min.css"
         />
 
-        <Grid textAlign='center' style={{
-          marginTop: '10px'
-        }}>
+        <Grid style={{marginTop: '10px'}}>
+          <Grid.Row columns={2}>
+            <Grid.Column width={2}>
+              <h4>Who are you?</h4>
+            </Grid.Column>
+
+            <Grid.Column>
+              hey
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
+        <Divider />
+
+        <Grid textAlign='center' style={{marginTop: '10px'}}>
           <Grid.Row columns={2}>
             <Grid.Column>
               <h1>UNIVERSE COMMAND</h1>
@@ -251,40 +333,38 @@ class Universe extends Component {
               />
             </Grid.Column>
           </Grid.Row>
-        </Grid>
 
-        <Divider />
+          <Grid.Row columns={2}>
+            <Grid.Column width={5}>
+              <Input
+                action={{
+                  content: 'Get Cosmo',
+                  onClick: () => this.handleGetCosmo()
+                }}
+                defaultValue={this.state.cosmoIndex}
+                onChange={this.changeGetCosmoInput}
+                style={{
+                  width: '100px'
+                }}
+              />
+            </Grid.Column>
 
-        <Grid textAlign='center' columns={2}>
-          <Grid.Column width={5}>
-            <Input
-              action={{
-                content: 'Get Cosmo',
-                onClick: () => this.handleGetCosmo()
-              }}
-              defaultValue={this.state.cosmoIndex}
-              onChange={this.changeGetCosmoInput}
-              style={{
-                width: '100px'
-              }}
-            />
-          </Grid.Column>
-
-          <Grid.Column>
-            <Input
-              fluid
-              disabled
-              defaultValue={this.state.cosmosAddress}
-              action={{
-                color: 'teal',
-                icon: 'copy',
-                content: 'Copy'
-              }}
-              style={{
-                width: '600px'
-              }}
-            />
-          </Grid.Column>
+            <Grid.Column>
+              <Input
+                fluid
+                disabled
+                defaultValue={this.state.cosmosAddress}
+                action={{
+                  color: 'teal',
+                  icon: 'copy',
+                  content: 'Copy'
+                }}
+                style={{
+                  width: '600px'
+                }}
+              />
+            </Grid.Column>
+          </Grid.Row>
         </Grid>
 
         <Divider />
@@ -292,7 +372,7 @@ class Universe extends Component {
         <Grid textAlign='center'>
           <Grid.Row columns={2}>
             <Grid.Column>
-              <h3>COSMONUTS</h3>
+              <h2>COSMONUTS</h2>
             </Grid.Column>
 
             <Grid.Column>
@@ -322,16 +402,115 @@ class Universe extends Component {
           </Grid.Row>
         </Grid>
 
+        <Grid textAlign='center'>
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <h3>Butter Creation</h3>
+            </Grid.Column>
+
+            <Grid.Column>
+              <h3>Seed Creation</h3>
+            </Grid.Column>
+          </Grid.Row>
+
+          <Grid.Row columns={2}>
+            <Grid.Column>
+              <Form onSubmit={this.createButter}>
+                <Form.Input
+                  inline
+                  name='nutId'
+                  label='Nut Id#'
+                  defaultValue={this.state.nutId}
+                  style={{width: '100px'}}
+                  onChange={this.butterFormChange}
+                />
+
+                <Form.Input
+                  label='Secret Hash'
+                  value='0xb68fe43f0d1a0d7aef123722670be50268e15365401c442f8806ef83b612976b'
+                  style={{width: '550px'}}
+                />
+
+                <Form.Input inline label='Matter Contributed' value='0' style={{width: '150px'}} />
+                <Form.Input inline label='Matter Draw Rate' value='0' style={{width: '150px'}} />
+
+                <Form.Input
+                  inline
+                  label='CID Path'
+                  value='https://ipfs.io/ipfs/__ipfsPath__'
+                  style={{width: '400px'}}
+                />
+
+                <Form.Input
+                  label='Signature'
+                  value='0x1915322bb77cd62486904890606dacba59ebca71d5a3b7b9a6a2dd87acba65c876298cab2f73c063f74058249f3ee68a0564559fd023e249638a0747799999531c'
+                  style={{width: '1060px'}}
+                />
+
+                <Button
+                  primary
+                  type='submit'
+                  content='Create Butter'
+                />
+              </Form>
+            </Grid.Column>
+
+            <Grid.Column>
+              <Form>
+                <Form.Input inline label='Nut Id#' placeholder='0' style={{width: '100px'}} />
+
+                <Form.Input
+                  label='Secret Hash'
+                  placeholder='0xb68fe43f0d1a0d7aef123722670be50268e15365401c442f8806ef83b612976b'
+                  style={{width: '550px'}}
+                />
+
+                <Form.Input
+                  inline
+                  label='CID Path'
+                  placeholder='https://ipfs.io/ipfs/__ipfsPath__'
+                  style={{width: '400px'}}
+                />
+
+                <Form.Input
+                  label='Signature'
+                  placeholder='0x1915322bb77cd62486904890606dacba59ebca71d5a3b7b9a6a2dd87acba65c876298cab2f73c063f74058249f3ee68a0564559fd023e249638a0747799999531c'
+                  style={{width: '1060px'}}
+                />
+
+                <Button
+                  primary
+                  type='submit'
+                  content='Create Seed'
+                />
+              </Form>
+            </Grid.Column>
+          </Grid.Row>
+        </Grid>
+
         <Divider />
 
         <Grid textAlign='center' columns={2}>
           <Grid.Row>
             <Grid.Column>
-              <h1>MATTER SYSTEMS</h1>
+              <h2>MATTER SYSTEMS</h2>
             </Grid.Column>
 
             <Grid.Column>
-              <Statistic label='Matter Address' value={this.state.matterAddress} size='mini' />
+              <Input
+                fluid
+                disabled
+                label='Matter'
+                defaultValue={this.state.matterAddress}
+                action={{
+                  color: 'teal',
+                  icon: 'copy',
+                  content: 'Copy'
+                }}
+                style={{
+                  width: '600px'
+                }}
+              />
             </Grid.Column>
           </Grid.Row>
 
@@ -351,6 +530,7 @@ class Universe extends Component {
             <Statistic label='Total Matter' value={this.state.totalMatter} size='mini' />
             <Statistic label='Treasury Matter' value={this.state.treasuryMatter} size='mini' />
             <Statistic label='System Matter' value={this.state.systemMatter} size='mini' />
+            <Statistic label='Owner Matter' value={this.state.ownerMatter} size='mini' />
           </Grid.Row>
 
           <Grid.Row>
@@ -416,6 +596,14 @@ class Universe extends Component {
           <Grid.Row>
             <Statistic label='wei' value={this.state.price} size='mini' />
             <Statistic label='wei/matter' value={this.state.rate} size='mini' />
+          </Grid.Row>
+
+          <Grid.Row>
+            <Statistic label='Butter Jars' value={this.state.butterJars} size='mini' />
+            <Statistic label='Butter Weight' value={this.state.butterWeight} size='mini' />
+            <Statistic label='Butter Spread' value={this.state.butterSpread} size='mini' />
+            <Statistic label='Seeds Created' value={this.state.seedsCreated} size='mini' />
+            <Statistic label='Seeds Grown' value={this.state.seedsGrown} size='mini' />
           </Grid.Row>
         </Grid>
 
